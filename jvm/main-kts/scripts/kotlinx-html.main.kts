a638b48764c7b7701e7d9f30e3ddb221b6ee3a26
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2021-08-26 14:03:25 +0200

    Update kotlinx-html.main.kts

--- jvm/main-kts/scripts/kotlinx-html.main.kts
+++ jvm/main-kts/scripts/kotlinx-html.main.kts
@@ -1,6 +1,6 @@
 #!/usr/bin/env kotlin
 
-@file:Repository("https://repo1.maven.org/maven2")
+@file:Repository("https://repo.maven.apache.org/maven2")
 @file:DependsOn("org.jetbrains.kotlinx:kotlinx-html-jvm:0.6.11")
 
 import kotlinx.html.*; import kotlinx.html.stream.*; import kotlinx.html.attributes.*
