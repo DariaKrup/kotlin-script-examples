4439e210ecc3a9445ab883513c78809e3c11d72c
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2023-10-04 11:14:20 +0200

    update konlinx-html-jvm version

--- jvm/main-kts/scripts/kotlin-html.kts
+++ jvm/main-kts/scripts/kotlin-html.kts
@@ -1,7 +1,7 @@
 #!/usr/bin/env kotlin
 
 @file:Repository("https://repo1.maven.org/maven2")
-@file:DependsOn("org.jetbrains.kotlinx:kotlinx-html-jvm:0.6.11")
+@file:DependsOn("org.jetbrains.kotlinx:kotlinx-html-jvm:0.9.1")
 
 import kotlinx.html.*; import kotlinx.html.stream.*; import kotlinx.html.attributes.*
 
